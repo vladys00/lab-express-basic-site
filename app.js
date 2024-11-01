58154c344c0b6380efa2d23204ef80e4fed0301b
Vladys K <vldk0022@gmail.com>
2024-11-01 17:45:36 +0100

    wprk/about/home done, need to adjust images

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.get("/home",(request,response,next)=>
 response.sendFile(__dirname + "/views/index.html"));
 
 
+
 app.get("/about",(request,response,next)=>
     response.sendFile(__dirname + "/views/about.html"));
 
